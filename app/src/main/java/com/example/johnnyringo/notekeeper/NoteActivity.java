19948ee350341773768ce4778dfbf95b57501d8e
VampiroJudo <johnconley507@hotmail.com>
2018-12-26 15:35:09 -0500

    removing unneeded import

--- app/src/main/java/com/example/johnnyringo/notekeeper/NoteActivity.java
+++ app/src/main/java/com/example/johnnyringo/notekeeper/NoteActivity.java
@@ -2,7 +2,6 @@ package com.example.johnnyringo.notekeeper;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
