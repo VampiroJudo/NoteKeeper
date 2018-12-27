bad88c9ecd146b24c932ccc55d80516a282642f9
VampiroJudo <johnconley507@hotmail.com>
2018-12-27 10:10:48 -0500

    adding methods to save previous note values

--- app/src/main/java/com/example/johnnyringo/notekeeper/NoteActivity.java
+++ app/src/main/java/com/example/johnnyringo/notekeeper/NoteActivity.java
@@ -23,6 +23,9 @@ public class NoteActivity extends AppCompatActivity {
     private EditText mTextNoteText;
     private int mNotePosition;
     private boolean mIsCancelling;
+    private String mOriginalNoteCourseId;
+    private String mOriginalNoteTitle;
+    private String mOriginalNoteText;
 
 
     @Override
@@ -41,6 +44,7 @@ public class NoteActivity extends AppCompatActivity {
         mSpinnerCourses.setAdapter(adapterCourses);
 
         readDisplayStateValues();
+        SaveOriginalNoteValues();
 
 
         mTextNoteTitle = findViewById(R.id.text_note_title);
@@ -50,16 +54,33 @@ public class NoteActivity extends AppCompatActivity {
             displayNote(mSpinnerCourses, mTextNoteTitle, mTextNoteText);
     }
 
+    private void SaveOriginalNoteValues() {
+        if(mIsNewNote)
+            return;
+        mOriginalNoteCourseId = mNote.getCourse().getCourseId();
+        mOriginalNoteTitle = mNote.getTitle();
+        mOriginalNoteText = mNote.getText();
+    }
+
     @Override
     protected void onPause() {
         super.onPause();
         if(mIsCancelling) {
             if(mIsNewNote) {
                 DataManager.getInstance().removeNote(mNotePosition);
+            } else {
+                storePreviousNoteValues();
             }
         } else {
             saveNote();
         }
+    }
+
+    private void storePreviousNoteValues() {
+        CourseInfo course = DataManager.getInstance().getCourse(mOriginalNoteCourseId);
+        mNote.setCourse(course);
+        mNote.setTitle(mOriginalNoteTitle);
+        mNote.setText(mOriginalNoteText);
     }
 
     private void saveNote() {
